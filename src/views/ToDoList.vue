41217cff715a0c1b27f9d73e702933c5283cd2b5
kelvinLin9 <kelvin80121@hotmail.com>
2023-01-30 10:16:46 +0800

    update

--- src/views/ToDoList.vue
+++ src/views/ToDoList.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="d-flex test mb-5">
+      <img src="../assets/images/logo.png" alt="">
+      <h1 class="ms-auto">{{ }}的待辦</h1>
+      <a href="#">登出</a>
+    </div>
+    <div class="card input">
+      <input class="text" type="text" placeholder="請輸入待辦事項" />
+      <a href="#" class="btn_add">+</a>
+    </div>
   </div>
 </template>
