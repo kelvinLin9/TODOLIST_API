b8b4fda3a1288ddad9281f884739e1c08e3a3d0e
kelvinLin9 <kelvin80121@hotmail.com>
2023-02-02 18:58:47 +0800

    修正上傳github-page問題

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // assetsDir: 'static', // 處理音檔問題
+  // 修正檔案命名問題
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/TODOLIST_API/' : '/'
 })
