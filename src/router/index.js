f2a760d6cde215d212e455e6a10923775acd3126
kelvinLin9 <kelvin80121@hotmail.com>
2023-02-03 18:31:31 +0800

    移除多於程式

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,15 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/UserLogin.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: () => import('../views/UserLogin.vue')
   },
   {
     path: '/ToDoList',
     name: 'ToDoList',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/ToDoList.vue')
+    component: () => import('../views/ToDoList.vue')
   }
 ]
 
